f90533622b621ef0806ac02810bf5dd254290f9d
Clark Gaebel <cg.wowus.cg@gmail.com>
2011-01-11 10:32:25 -0500

    Some basic cleanup.

--- pipe_test.c
+++ pipe_test.c
@@ -1,3 +1,25 @@
+/*
+ * The MIT License
+ * Copyright (c) 2011 Clark Gaebel <cg.wowus.cg@gmail.com>
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 #include "pipe.h"
 
 #include <pthread.h>
@@ -30,8 +52,9 @@
     ? memcmp(a1, a2, sizeof(a1) == 0) \
     : 0)
 
-#define array_eq_len(type, a1, a2, len2) \
-    (sizeof(a1) == sizeof(type)*(len2)   \
+
+#define array_eq_len(a1, a2, len2) \
+    (sizeof(a1) == sizeof((a2)[0])*(len2)   \
     ? memcmp(a1, a2, sizeof(a1)) == 0    \
     : 0)
 
@@ -65,8 +88,8 @@ DEF_TEST(basic_storage)
         8, 7, 6, 5
     };
 
-    assert(array_eq_len(int, expecteda, bufa, acnt));
-    assert(array_eq_len(int, expectedb, bufb, bcnt));
+    assert(array_eq_len(expecteda, bufa, acnt));
+    assert(array_eq_len(expectedb, bufb, bcnt));
 
     pipe_consumer_free(c);
 }
--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 clang --analyze pipe.c pipe_test.c
-gcc -g -fstrict-aliasing -std=c99 -DDEBUG -pthread -O1 -pipe main.c pipe.c pipe_test.c
+gcc -g -fstrict-aliasing -std=c99 -DDEBUG -pthread -O -pipe main.c pipe.c pipe_test.c
 valgrind ./a.out
